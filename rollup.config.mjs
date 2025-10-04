7d3966f26273f54b45de200656eba373c5b60dfb
Mahad Munir <mahad@xconn.io>
2025-10-04 17:08:48 +0500

    rollup bundle

---
+++ rollup.config.mjs
@@ -0,0 +1,57 @@
+// rollup.config.mjs
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import json from '@rollup/plugin-json';
+import dts from 'rollup-plugin-dts';
+
+const input = 'lib/index.ts';
+
+export default [
+    {
+        input,
+        output: [
+            {
+                dir: 'ts-built',
+                format: 'esm',
+                entryFileNames: '[name].js',
+                sourcemap: true,
+            },
+            {
+                dir: 'ts-built',
+                format: 'cjs',
+                entryFileNames: '[name].cjs',
+                sourcemap: true,
+                exports: 'auto',
+            },
+        ],
+        plugins: [
+            replace({
+                preventAssignment: true,
+                'process.env.NODE_ENV': JSON.stringify('production'),
+            }),
+            nodeResolve({
+                browser: true,
+                preferBuiltins: true,
+            }),
+            commonjs(),
+            json(),
+            typescript({
+                tsconfig: './tsconfig.json',
+                declaration: false,
+            }),
+        ],
+        external: [], // Add your externals here
+    },
+
+    // DTS output
+    {
+        input,
+        output: {
+            file: 'ts-built/index.d.ts',
+            format: 'es',
+        },
+        plugins: [dts()],
+    },
+];
